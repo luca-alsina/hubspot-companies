49015099ab6850fb7583170749ad3002716c3318
Luca ALSINA <contact@alsinaluca.fr>
2023-05-15 16:35:35 +0200

    Modification visuelles sur le modal

--- resources/js/Pages/Companies/Partials/ShowCompanyModal.vue
+++ resources/js/Pages/Companies/Partials/ShowCompanyModal.vue
@@ -68,20 +68,20 @@ watch(() => props.company, () => {
 
         <div class="px-6 py-4">
             <div class="text-lg font-medium text-gray-900">
-                <h2>{{ company.name }}</h2>
+                <h1>{{ company.name }}</h1>
             </div>
 
             <div class="row-auto flex justify-between">
                 <div class="flex-col">
                     <div class="mt-4 text-sm text-gray-600">
-                        <h3><a href="{{ company.website }}">{{ company.website }}</a></h3>
+                        <h3><a class="underline" href="{{ company.website }}">{{ company.website }}</a></h3>
                         <h3><a href="tel:{{ company.phone }}">{{ company.phone }}</a></h3>
                     </div>
 
                     <div class="flex flex-row justify-between mt-4">
-                        <a href="mailto:{{ company.email }}" v-if="company.email" class="rounded-full bg-blue-500 hover:bg-blue-600 text-white w-12 h-12 flex items-center justify-center">
+                        <a href="mailto:{{ company.email }}" v-if="company.email" class="rounded-full bg-blue-500 hover:bg-blue-600 text-white w-8 h-8 flex items-center justify-center">
                             <svg class="p-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <rect x="3" y="5" width="18" height="14" rx="2" stroke="#000000" stroke-width="2" stroke-linecap="round"></rect> </g></svg>                </a>
-                        <a href="tel:{{ company.phone }}" v-if="company.phone" class="rounded-full bg-green-500 hover:bg-green-600 text-white w-12 h-12 flex items-center justify-center">
+                        <a href="tel:{{ company.phone }}" v-if="company.phone" class="rounded-full bg-green-500 hover:bg-green-600 text-white w-8 h-8 flex items-center justify-center">
                             <svg class="p-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Communication / Phone"> <path id="Vector" d="M9.50246 4.25722C9.19873 3.4979 8.46332 3 7.64551 3H4.89474C3.8483 3 3 3.8481 3 4.89453C3 13.7892 10.2108 21 19.1055 21C20.1519 21 21 20.1516 21 19.1052L21.0005 16.354C21.0005 15.5361 20.5027 14.8009 19.7434 14.4971L17.1069 13.4429C16.4249 13.1701 15.6483 13.2929 15.0839 13.7632L14.4035 14.3307C13.6089 14.9929 12.4396 14.9402 11.7082 14.2088L9.79222 12.2911C9.06079 11.5596 9.00673 10.3913 9.66895 9.59668L10.2363 8.9163C10.7066 8.35195 10.8305 7.57516 10.5577 6.89309L9.50246 4.25722Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
                         </a>
                     </div>
@@ -107,12 +107,12 @@ watch(() => props.company, () => {
 
             <!-- Liste des contacts -->
             <div class="mt-4 text-sm text-gray-600">
-                <h3>Contacts</h3>
+                <h2 class="text-md">Contacts</h2>
 
-                <div v-for="contact in company.contacts">
+                <div v-for="contact in company.contacts" class="p-2 border border-gray-500 mt-1">
                     <h3>{{ contact.first_name }} {{ contact.last_name }}</h3>
-                    <h3>{{ contact.email }}</h3>
-                    <h3>{{ contact.phone }}</h3>
+                    <h3 v-if="contact.email"><a href="mailto:{{ contact.email }}">{{ contact.email }}</a></h3>
+                    <h3 v-if="contact.phone"><a href="tel:{{ contact.phone }}">{{ contact.phone }}</a></h3>
                 </div>
             </div>
 
@@ -120,7 +120,7 @@ watch(() => props.company, () => {
 
         <div class="flex flex-row justify-center px-6 py-4 bg-gray-100 text-right">
             <template v-if="isLoading">
-                <span class="mr-2">Loading...</span>
+                <span class="mr-2">Chargement des données...</span>
                 <svg class="animate-spin h-5 w-5 mr-2 inline-block text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM12 20a8 8 0 008-8h-4a4 4 0 01-4 4v4zm5.657-9.657A6 6 0 1012 2.343v4.243a3 3 0 014.243 0l1.414-1.414z"></path>
